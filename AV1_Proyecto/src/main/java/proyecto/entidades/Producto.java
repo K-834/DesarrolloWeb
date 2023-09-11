5dc96bf512f11048a0a72a1e48ca0e57c5d9d177
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-09-11 18:48:08 -0500

    Crud Productos - JSP y Servlet

---
+++ AV1_Proyecto/src/main/java/proyecto/entidades/Producto.java
@@ -0,0 +1,67 @@
+package proyecto.entidades;
+
+/**
+ *
+ * @author aferz
+ */
+public class Producto {
+    
+    private String codigo;
+    private String nombre;
+    private String marca;
+    private String modelo;
+    private double precio;
+
+    public Producto() {
+        
+    }
+
+    public Producto(String codigo, String nombre, String marca, String modelo, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
